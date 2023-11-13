0817cf498e66d7f5e4207a0c967e7d03fd6153d6
many-to-one <xllxlex@gmail.com>
2023-11-13 18:37:48 +0100

    users.urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from . import views
-
+from .views import *
+from django.contrib import admin
 
 urlpatterns = [
-    # path('auth/', views.index, name='index'),
+    path('register/', UserRegistrationView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/', UserListView.as_view(), name='users'),
+    path('accessToken/', AccesToken.as_view(), name='accessToken'),
 ]
