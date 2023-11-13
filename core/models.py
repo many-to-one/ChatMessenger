8f7f54ff2ca1f96f52c48393d4743ebee94c9fdc
many-to-one <xllxlex@gmail.com>
2023-11-13 18:41:00 +0100

    core.views.py: Message, Chat, Conversation, OutcomingFriendRequest, IncomingFriendRequest, BlockedUsers models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,109 @@
 from django.db import models
+from users.models import CustomUser
 
-# Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    resend = models.BooleanField(default=False)
+    unread = models.BooleanField(default=True)
+    chat = models.ForeignKey(
+        'Chat', 
+        on_delete=models.CASCADE, 
+        null=True
+        )
+    conversation = models.ForeignKey(
+        'Conversation', 
+        on_delete=models.CASCADE, 
+        null=True
+        )
+
+    def __str__(self):
+        return self.content
+    
+
+class Chat(models.Model):
+    name = models.CharField(
+        null=True,
+        max_length=50
+    )
+    user = models.ManyToManyField(CustomUser)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    creater = models.CharField(
+        null=True,
+        max_length=50
+    )
+    def __str__(self):
+        return self.name
+    
+    def usernames(self):
+        return [user.username for user in self.user.all()]
+
+    def user_info(self):
+        user_data = []
+        for user in self.user.all():
+            user_info = {
+                'id': user.id,
+                'username': user.username,
+                'photo': f'/media/{user.photo}' if user.photo else None
+            }
+            user_data.append(user_info)
+        return user_data
+    
+
+class Conversation(models.Model):
+    user = models.ManyToManyField(CustomUser)
+
+    def __str__(self):
+        return str(self.id)
+    
+
+class OutcomingFriendRequest(models.Model):
+    user = models.ForeignKey(
+        CustomUser, 
+        null=True,
+        on_delete=models.CASCADE, 
+        related_name='sender',
+        )
+    outUsers = models.ManyToManyField(
+        CustomUser, 
+        related_name='receivers'
+        )
+
+    
+    def __str__(self):
+        return str(self.user)
+    
+
+class IncomingFriendRequest(models.Model):
+    user = models.ForeignKey(
+        CustomUser, 
+        null=True,
+        on_delete=models.CASCADE, 
+        related_name='receiver',
+        )
+    inUsers = models.ManyToManyField(
+        CustomUser, 
+        related_name='senders'
+        )
+
+    
+    def __str__(self):
+       return str(self.user)
+    
+
+class BlockedUsers(models.Model):
+    user = models.ForeignKey(
+        CustomUser, 
+        null=True,
+        on_delete=models.CASCADE, 
+        related_name='blockingUser',
+        )
+    blockedUsers = models.ManyToManyField(
+        CustomUser, 
+        related_name='blockedUsers'
+        )
+
+    
+    def __str__(self):
+        return str(self.user)
