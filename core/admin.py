6b2f569748dc2c444a2cc7d2efef131a7a0262c1
many-to-one <xllxlex@gmail.com>
2023-11-13 18:47:04 +0100

    core.admin.py

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
+from users.models import FriendList
 
-# Register your models here.
+admin.site.register(Message)
+admin.site.register(Chat)
+admin.site.register(Conversation)
+admin.site.register(FriendList)
+admin.site.register(IncomingFriendRequest)
+admin.site.register(OutcomingFriendRequest)
+admin.site.register(BlockedUsers)
