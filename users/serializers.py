4393fdf2d806767fcb504a08f066c2c691c1bf2a
many-to-one <xllxlex@gmail.com>
2023-11-13 18:33:36 +0100

    users.serializers.py

---
+++ users/serializers.py
@@ -0,0 +1,51 @@
+from rest_framework import serializers
+from .models import CustomUser
+
+class RegisterSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': 'password'})
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'username',
+            'email',
+            'password'
+        ]
+    
+
+class LoginSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(style={'input_type': 'password'})
+
+    class Meta:
+        model = CustomUser
+        fields = [
+            'email',
+            'password',
+            'login_at',
+        ]
+
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'username',
+            'email',
+            'photo',
+        ]
+
+    
+class AccessTokenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = [
+            'id',
+            'username',
+            'email',
+            'photo',
+            'token',
+        ]
