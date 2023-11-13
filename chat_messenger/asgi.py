4dda393a11eb7da33243dec76161aa53b8438d45
many-to-one <xllxlex@gmail.com>
2023-11-13 19:32:42 +0100

    core.asgi.py

--- chat_messenger/asgi.py
+++ chat_messenger/asgi.py
@@ -1,16 +1,41 @@
 """
-ASGI config for chat_messenger project.
+ASGI config for qwe project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+from core.consumers import *
+from core import routing
+from django.urls import path
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_messenger.settings')
+# django.setup()
 
-application = get_asgi_application()
+# Import Django settings after configuring the module
+from django.conf import settings
+
+# Now you can access Django settings
+print(settings.DEBUG)
+
+
+
+
+
+
+
+
+
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": 
+        AuthMiddlewareStack(URLRouter(routing.websocket_urlpatterns)),
+})
